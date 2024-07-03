83c0f26dbde3b836548ed91da7e565aa162e7dc1
jamesbrobb <jamesbenrobb@gmail.com>
2024-07-03 19:32:20 +0100

    removing and set class properties, methods and accessors from members

--- src/declarations/definitions/class.ts
+++ src/declarations/definitions/class.ts
@@ -11,6 +11,7 @@ import {HeritageClause} from "./heritage";
 import {getModifiers, Modifiers} from "./modifiers";
 
 
+
 export type ClassElement = PropertyDeclaration | MethodDeclaration | GetAccessorDeclaration | SetAccessorDeclaration | ConstructorDeclaration
 
 
@@ -32,13 +33,56 @@ export const classDeclarationDefinition: DeclarationDefinition<ClassDeclaration>
     modifiers: getModifiers
   },
   postProcess: [
-    (dec: ClassDeclaration) => {
-      dec.properties = dec.members.filter(isPublicPropertyDeclaration)
-      dec.methods = dec.members.filter(isPublicMethodDeclaration)
-      dec.accessors = [
-        ...dec.members.filter(isPublicSetAccessorDeclaration),
-        ...dec.members.filter(isPublicGetAccessorDeclaration)
-      ]
-    }
+    addProperties,
+    addMethods,
+    addAccessors,
+    filterMembers
   ]
+}
+
+
+export function isClassDeclaration(dec: DeclarationKind<any>): dec is ClassDeclaration {
+  return dec.kind === ts.SyntaxKind[ts.SyntaxKind.ClassDeclaration];
+}
+
+
+function addProperties(dec: ClassDeclaration): ClassDeclaration {
+  const properties = dec.members.filter(isPublicPropertyDeclaration);
+
+  if(properties && properties.length > 0) {
+    dec.properties = properties
+  }
+
+  return dec;
+}
+
+function addMethods(dec: ClassDeclaration): ClassDeclaration {
+  const methods = dec.members.filter(isPublicMethodDeclaration);
+
+  if(methods && methods.length > 0) {
+    dec.methods = methods
+  }
+
+  return dec;
+}
+
+function addAccessors(dec: ClassDeclaration): ClassDeclaration {
+  const accessors = [
+    ...dec.members.filter(isPublicSetAccessorDeclaration),
+    ...dec.members.filter(isPublicGetAccessorDeclaration)
+  ]
+
+  if(accessors && accessors.length > 0) {
+    dec.accessors = accessors
+  }
+
+  return dec;
+}
+
+function filterMembers(dec: ClassDeclaration): ClassDeclaration {
+  dec.members = dec.members.filter(arg => !dec.properties?.includes(arg as any))
+    .filter(arg => !dec.methods?.includes(arg as any))
+    .filter(arg => !dec.accessors?.includes(arg as any));
+
+  return dec;
 }
