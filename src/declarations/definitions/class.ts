f5364e30ce7d534442a5cf732bb8ac1c27d3c5bc
jamesbrobb <jamesbenrobb@gmail.com>
2024-07-03 10:55:08 +0100

    added additional properties to class declaration for public properties, methods and accessors

--- src/declarations/definitions/class.ts
+++ src/declarations/definitions/class.ts
@@ -1,10 +1,10 @@
 import * as ts from "typescript";
 import {DeclarationDefinition} from "../declaration-definition.types";
 import {DeclarationKind} from "../declaration-kind.types";
-import {PropertyDeclaration} from "./property";
-import {MethodDeclaration} from "./method";
-import {GetAccessorDeclaration} from "./get-accessor";
-import {SetAccessorDeclaration} from "./set-accessor";
+import {isPublicPropertyDeclaration, PropertyDeclaration} from "./property";
+import {isPublicMethodDeclaration, MethodDeclaration} from "./method";
+import {GetAccessorDeclaration, isPublicGetAccessorDeclaration} from "./get-accessor";
+import {isPublicSetAccessorDeclaration, SetAccessorDeclaration} from "./set-accessor";
 import {ConstructorDeclaration} from "./constructor";
 import {TypeParameterDeclaration} from "./type";
 import {HeritageClause} from "./heritage";
@@ -15,11 +15,14 @@ export type ClassElement = PropertyDeclaration | MethodDeclaration | GetAccessor
 
 
 export type ClassDeclaration = {
-  name: string,
-  typeParameters?: TypeParameterDeclaration[],
-  heritage?: HeritageClause[],
-  members: ClassElement[],
+  name: string
+  typeParameters?: TypeParameterDeclaration[]
+  heritage?: HeritageClause[]
+  members: ClassElement[]
   modifiers?: Modifiers
+  properties?: PropertyDeclaration[]
+  accessors?: (GetAccessorDeclaration | SetAccessorDeclaration)[]
+  methods?: MethodDeclaration[]
 } & DeclarationKind<ts.ClassDeclaration>
 
 
@@ -27,5 +30,15 @@ export const classDeclarationDefinition: DeclarationDefinition<ClassDeclaration>
   props: ['name', 'typeParameters', 'members', 'heritageClauses', 'modifiers'],
   propHandlers: {
     modifiers: getModifiers
-  }
+  },
+  postProcess: [
+    (dec: ClassDeclaration) => {
+      dec.properties = dec.members.filter(isPublicPropertyDeclaration)
+      dec.methods = dec.members.filter(isPublicMethodDeclaration)
+      dec.accessors = [
+        ...dec.members.filter(isPublicSetAccessorDeclaration),
+        ...dec.members.filter(isPublicGetAccessorDeclaration)
+      ]
+    }
+  ]
 }
